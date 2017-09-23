5fa38c76a1d16884f417b2e332148ac570aaf1e8
andrewallen <andrewallen@CSCT2552327X003.ad.georgiasouthern.edu>
2017-09-23 16:02:02 -0400

    A2

--- A2/src/assignment/MyAthleticProgram.java
+++ A2/src/assignment/MyAthleticProgram.java
@@ -4,6 +4,7 @@ public class MyAthleticProgram {
 
 	public static void main(String[] args) {
 		//Create a Swimmer
+		/*
 		Swimmer s = new Swimmer("Missy Franklin", 1995, 5, 10);
 		s.setGender('F');
 		Swimmer.Event[] e = {Swimmer.Event.Backstroke,Swimmer.Event.Freestyle,Swimmer.Event.Medley};
@@ -11,13 +12,14 @@ public class MyAthleticProgram {
 		s.setTeamName("Colorado Stars");
 		s.setIsAPro(false);
 		System.out.println(s);
-		
+		*/
 		//Create a golfer
+		/*
 		Golfer g = new Golfer("Tiger Woods", 1975, 12, 30 );
 		g.setGender('m');
 		g.setIsAPro(true);
 		System.out.println(g);
-	
+	   */
 		
 	}
 
